3116d990b0985d2c419f89c3845ca9e01f13e2bd
Timo-China <dingyas@hotmai.com>
2018-05-23 16:58:27 +0800

    整理代码

--- OSGQtDemo/OSGViewWidget.cpp
+++ OSGQtDemo/OSGViewWidget.cpp
@@ -1,33 +1,29 @@
 #include "OSGViewWidget.h"
-#include <osgViewer/ViewerEventHandlers>
 #include <osgGA/TrackballManipulator>
-#include <osgDB/ReadFile>
 
 
 OSGViewWidget::OSGViewWidget(QWidget* parent /*= 0*/)
     :osgQt::GLWidget(parent)
 {
+    this->setThreadingModel(osgViewer::ViewerBase::SingleThreaded);
     this->_gw = new osgQt::GraphicsWindowQt(this);
+
 //     osg::ref_ptr<osg::Camera> camera = new osg::Camera;
-//     camera->setGraphicsContext(_gw);
+//     camera->setGraphicsContext(this->_gw);
 //     camera->setClearColor( osg::Vec4(0.2, 0.2, 0.6, 1.0) );
 //     camera->setClearDepth(1.0);
 //     camera->setViewport( new osg::Viewport(0, 0, _gw->getTraits()->width, _gw->getTraits()->height) );
-//     setCamera(camera);
+//     GLenum buffer = _gw->getTraits()->doubleBuffer ? GL_BACK : GL_FRONT;
+//     camera->setDrawBuffer(buffer);
+//     camera->setReadBuffer(buffer);
+//     this->setCamera(camera.get());
 
     this->getCamera()->setGraphicsContext(_gw);
     this->getCamera()->setViewport( new osg::Viewport(0, 0, _gw->getTraits()->width, _gw->getTraits()->height));
 
-    addEventHandler( new osgViewer::StatsHandler );
-    this->setThreadingModel(osgViewer::ViewerBase::SingleThreaded);
-
     osgGA::TrackballManipulator* trackBallMpl = new osgGA::TrackballManipulator();
     setCameraManipulator(trackBallMpl);
 
-    // default scene
-    osg::ref_ptr<osg::Group> root = new osg::Group;
-    root->addChild(osgDB::readNodeFile("cow.osg"));
-    this->setSceneData(root);
 }
 
 
@@ -43,8 +39,7 @@ void OSGViewWidget::resizeEvent(QResizeEvent* event)
     {
         getCamera()->setViewport(0 ,0, _gw->getTraits()->width, _gw->getTraits()->height);
         getCamera()->setProjectionMatrixAsPerspective(
-            30.0f, static_cast<double>(_gw->getTraits()->width)/static_cast<double>(_gw->getTraits()->height), 1.0f, 10000.0f );
-
+            30.f, static_cast<double>(_gw->getTraits()->width)/static_cast<double>(_gw->getTraits()->height), 1.0f, 10000.0f );
     }
 
     frame();
